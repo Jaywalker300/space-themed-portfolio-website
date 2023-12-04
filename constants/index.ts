906997c905aa9483af638cebf77cb6b502a34464
jaywalker <chiketravis300@gmail.com>
2023-12-05 00:43:07 +0100

    first commit

---
+++ constants/index.ts
@@ -0,0 +1,311 @@
+export const Skill_data = [
+    {
+      skill_name: "Html 5",
+      Image: "/html.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Css",
+      Image: "/css.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Java Script",
+      Image: "/js.png",
+      width: 65,
+      height: 65,
+    },
+    {
+      skill_name: "Tailwind Css",
+      Image: "/tailwind.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "React",
+      Image: "/react.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Redux",
+      Image: "/redux.png",
+      width: 80,
+      height: 80,
+    },
+
+    {
+      skill_name: "Type Script",
+      Image: "/ts.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Next js 13",
+      Image: "/next.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Framer Motion",
+      Image: "/framer.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Stripe Payment",
+      Image: "/stripe.webp",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Node js",
+      Image: "/node-js.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Mongo db",
+      Image: "/mongodb.png",
+      width: 40,
+      height: 40,
+    },
+  
+  ];
+  
+  export const Socials = [
+    {
+      name: "Discord",
+      src: "/instagram.svg",
+    },
+    {
+      name: "Facebook",
+      src: "/facebook.svg",
+    },
+    {
+      name: "Instagram",
+      src: "/discord.svg",
+    },
+  ];
+  
+  
+  
+  export const Frontend_skill = [
+    {
+      skill_name: "Html 5",
+      Image: "/html.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Css",
+      Image: "/css.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Java Script",
+      Image: "/js.png",
+      width: 65,
+      height: 65,
+    },
+    {
+      skill_name: "Tailwind Css",
+      Image: "/tailwind.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Material UI",
+      Image: "/mui.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "React",
+      Image: "/react.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Redux",
+      Image: "/redux.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Type Script",
+      Image: "/ts.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Next js 13",
+      Image: "/next.png",
+      width: 80,
+      height: 80,
+    },
+  ];
+  
+  export const Backend_skill = [
+    {
+      skill_name: "Node js",
+      Image: "/node-js.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Express js",
+      Image: "/express.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Mongo db",
+      Image: "/mongodb.png",
+      width: 40,
+      height: 40,
+    },
+    {
+      skill_name: "Fire base",
+      Image: "/Firebase.png",
+      width: 55,
+      height: 55,
+    },
+    {
+      skill_name: "My SQL",
+      Image: "/mysql.png",
+      width: 70,
+      height: 70,
+    },
+    {
+      skill_name: "Prisma",
+      Image: "/prisma.webp",
+      width: 70,
+      height: 70,
+    },
+  ];
+  
+  export const Full_stack = [
+    {
+      skill_name: "React Native",
+      Image: "/ReactNative .png",
+      width: 70,
+      height: 70,
+    },
+    {
+      skill_name: "Docker",
+      Image: "/docker.webp",
+      width: 70,
+      height: 70,
+    },
+  
+    {
+      skill_name: "Figma",
+      Image: "/figma.png",
+      width: 50,
+      height: 50,
+    },
+  
+  ];
+  
+  
+  
+  export const Other_skill = [
+    {
+      skill_name: "Go",
+      Image: "/go.png",
+      width: 60,
+      height: 60,
+    },
+    {
+      skill_name: "adobe-illustrator",
+      Image: "/illustrator.png",
+      width: 60,
+      height: 60,
+    },
+    {
+      skill_name: "PhotoShop",
+      Image: "/Photoshop.png",
+      width: 60,
+      height: 60,
+    },
+    {
+      skill_name: "After-Effects",
+      Image: "/AfterEffects.png",
+      width: 60,
+      height: 60,
+    },
+    {
+      skill_name: "PremierPro",
+      Image: "/PremierPro.png",
+      width: 60,
+      height: 60,
+    },
+  ];
+
+  export const data = [
+      
+    {
+  
+      id:1,
+      Image:'/public/portfolio-items/ai-saas-website.jpg',
+      title: 'An AI Saas Page built with Next 13, Prisma, Tailwind, Typescript and shadcn for ui components',
+      github: 'https://github.com/Jaywalker300/sabiguy-saas-app',
+      demo: 'https://sabiguy-saas-qqvarfl65-jaywalker300.vercel.app',
+     },
+
+    {
+  
+      id:2,
+      Image:'/public/portfolio-items/Article Summarizer.jpg',
+      title: 'An Article summarizer built with OpenAI-GPT',
+      github: 'https://jay-ai-summarizer.netlify.app/',
+      demo: 'https://github.com/Jaywalker300/summarizer-ai-gpt.git',
+     },
+     
+     {
+       
+       id:3,
+       Image:'/public/portfolio-items/restaurant-menu -page.jpg',
+       title: 'Restaurant Menu Page',
+       github: 'https://github.com/Jaywalker300/menu-page',
+       demo: 'https://jaywalker300.github.io/menu-page/',
+      
+     },
+  
+     {
+  
+      id:4,
+      Image:'/public/portfolio-items/car-showcase.jpg',
+      title: 'Car rental web application built with NextJS',
+      github: 'https://github.com/Jaywalker300/CAR-SHOWCASE',
+      demo: 'https://car-showcase-5gao.vercel.app/'
+      
+     },
+       
+     {
+  
+      id:5,
+      Image: '/public/portfolio-items/stripe-landing-page.jpg',
+      title: 'Stripe Online Payment Landing page',
+      github: 'https://github.com/Jaywalker300/stripe-menu',
+      demo: ' https://jaywalker300.github.io/stripe-menu/',
+     },
+  
+      {
+  
+      id:6,
+      Image:'/public/portfolio-items/cocktail-database.jpg',
+      title: 'Cocktail Database Demo',
+      github: 'https://github.com',
+      demo: 'https://digitalocean.com/home',
+     },
+   ]
+  
+  
+  
+  
+  
