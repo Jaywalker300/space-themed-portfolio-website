cd0a92f60794bcb44676caf142b9daac69bc33e7
jaywalker <chiketravis300@gmail.com>
2023-12-05 22:33:40 +0100

    adjusted some typos

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -22,7 +22,7 @@ const HeroContent = () => {
 
             <motion.div variants={slideInFromLeft(0.5)}
             className='flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto'>
-            <span>Hello there👋 i'm <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to bg-cyan-500'>Oluwachike</span> Jaywalker
+            <span>Hello there👋 i&apos;m <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to bg-cyan-500'>Oluwachike</span> Jaywalker
             </span>
             </motion.div>
 
